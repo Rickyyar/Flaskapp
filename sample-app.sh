b73bb10c64a471486373db4115c0271af10aff2e
Rickyyar <145581819+Rickyyar@users.noreply.github.com>
2024-12-04 16:41:53 +0100

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -7,7 +7,7 @@ cleanup() {
     echo "Application folder has been removed"
 }
 python3 -m venv flask_venv
-source flask_venv/bin/activated
+source flask_venv/bin/activate
 git clone https://github.com/Rickyyar/Flaskapp.git
 pip install -r requirements.txt
 python3 sample_app.py
