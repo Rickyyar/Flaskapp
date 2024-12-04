e2b3d0dde9888b96b01ac14447cd6b65050ea324
Rickyyar <145581819+Rickyyar@users.noreply.github.com>
2024-12-04 16:43:55 +0100

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -9,5 +9,6 @@ cleanup() {
 python3 -m venv flask_venv
 source flask_venv/bin/activate
 git clone https://github.com/Rickyyar/Flaskapp.git
+cd Flaskapp/
 pip install -r requirements.txt
 python3 sample_app.py
