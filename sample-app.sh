d6250b9c8195dc294e1991f57a0c3ca7a7b5664d
Rickyyar <145581819+Rickyyar@users.noreply.github.com>
2024-12-04 16:36:51 +0100

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # staging exercise with jenkins
+cleanup() {
+    echo "Cleaning up ..."
+    deactivate
+    rm -rf /home/devasc/Documents/Git_testing/deployment/Flaskapp
+    echo "Application folder has been removed"
+}
 python3 -m venv Flaskapp
 source flask_venv/bin/activated
 git clone https://github.com/Rickyyar/Flaskapp.git
 pip install -r requirements.txt
 python3 sample_app.py
-deactivate
-cd ..
-rm -rf Flaskapp
