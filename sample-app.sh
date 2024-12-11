0b6824b999e8966ef5e4548da678c0eb4c0d46fb
Rickyyar <145581819+Rickyyar@users.noreply.github.com>
2024-12-11 10:07:53 +0100

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -8,9 +8,16 @@ cleanup() {
     echo "Application folder has been removed"
 }
 trap cleanup EXIT
+echo "$(date '+%H:%M:%S') - creating python VENV"
 python3 -m venv flask_venv
+sleep 15  # Pause for 2 seconds
+echo "$(date '+%H:%M:%S') - This is the message after a 15-second pause."²
+echo "$(date '+%H:%M:%S')- entering VENV"
 source flask_venv/bin/activate
+echo "$(date '+%H:%M:%S') - cloning git repository"
 git clone https://github.com/Rickyyar/Flaskapp.git
 cd Flaskapp/
 pip install -r requirements.txt
+echo "$(date '+%H:%M:%S') - starting python application"
 python3 sample_app.py
+echo "$(date '+%H:%M:%S') - application stopped"
